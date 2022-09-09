6f593cb747da5060eefa5b000afea4bded66a9f4
glebmiller <gleb.miller.ge@gmail.com>
2022-09-09 16:37:45 +0400

    lists task completed

--- exercises/lists.py
+++ exercises/lists.py
@@ -1,21 +1,44 @@
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
-        pass
+        def max_value(searched_list: list[int]) -> int:
+            res = -(10**6)
+            for i in searched_list:
+                if i > res:
+                    res = i
+            return res
+
+        if not input_list:
+
+            return input_list
+        else:
+            max_int = max_value(input_list)
+            input_list = list(map(lambda x: x if x < 0 else max_int, input_list))
+            return input_list
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
-        pass
+        def search(arr, low, high, x):
+
+            # Check base case
+            if high >= low:
+                mid = (high + low) // 2
+
+                # If element is present at the middle itself
+                if arr[mid] == x:
+                    return mid
+
+                # If element is smaller than mid, then it can only
+                # be present in left subarray
+                elif arr[mid] > x:
+                    return search(arr, low, mid - 1, x)
+
+                # Else the element can only be present in right subarray
+                else:
+                    return search(arr, mid + 1, high, x)
+
+            else:
+                # Element is not present in the array
+                return -1
+
+        return search(input_list, 0, len(input_list) - 1, query)
