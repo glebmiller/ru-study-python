a84995199c73a2fc8ddeb3a81689e58b1b8c1082
glebmiller <gleb.miller.ge@gmail.com>
2022-09-11 12:18:31 +0400

    added map.py

--- exercises/map.py
+++ exercises/map.py
@@ -1,29 +1,27 @@
 from typing import Union
+from functools import reduce
 
 
 class MapExercise:
     @staticmethod
     def rating(list_of_movies: list[dict]) -> float:
-        """
-        !!Задание нужно решить используя map!!
-        Посчитать средний рейтинг фильмов (rating_kinopoisk), у которых две или больше стран.
-        Фильмы у которых рейтинг не задан или равен 0 не учитывать в расчете среднего.
-        :param list_of_movies: Список фильмов.
-        Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
-        :return: Средний рейтинг фильмов у которых две или больше стран
-        """
-        pass
+        def func(movie: dict) -> float:
+            if "," in movie["country"] and movie["rating_kinopoisk"] not in ("", "0"):
+                return float(movie["rating_kinopoisk"])
+            else:
+                return None
+
+        list_of_movies = list(map(func, list_of_movies))
+        list_of_movies = list(filter(lambda x: x is not None, list_of_movies))
+        return reduce(lambda x, y: x + y, list_of_movies) / len(list_of_movies)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
-        """
-        !!Задание нужно решить используя map!!
-        Посчитать количество букв 'и' в названиях всех фильмов с рейтингом (rating_kinopoisk) больше
-        или равным заданному значению
-        :param list_of_movies: Список фильмов
-        Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
-        :param rating: Заданный рейтинг
-        :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
-        или равным заданному значению
-        """
-        pass
+        def count_i(movie: dict) -> int:
+            if movie["rating_kinopoisk"] != "" and float(movie["rating_kinopoisk"]) > rating:
+                return movie["name"].count("и")
+            else:
+                return 0
+
+        list_of_movies = map(count_i, list_of_movies)
+        return reduce(lambda x, y: x + y, list_of_movies)
