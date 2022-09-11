28a7ed5555638d95ba821980855b530ac3c2466a
glebmiller <gleb.miller.ge@gmail.com>
2022-09-11 15:34:36 +0400

    filter_map.py solved

--- exercises/filter_map.py
+++ exercises/filter_map.py
@@ -4,15 +4,6 @@ from typing import Any, Callable, List, Tuple
 class FilterMapExercise:
     @staticmethod
     def filter_map(func: Callable[[Any], Tuple[bool, Any]], input_array: List[Any]) -> List[Any]:
-        """
-        Реализовать функцию, которая ведет себя как filter и map. К каждому значению из
-        списка применяется функция, которая в ответ возвращает кортеж
-        (булево значение, результат работы функции,).
-        Если первый элемент кортежа истина, то результат добавляется в список.
+        res = [func(i)[1] for i in input_array if func(i)[0]]
 
-        Принимает в качестве аргументов функцию и итерируемый источник, а возвращает список.
-        :param func: Функция, применяемая к каждому элементу списка.
-        :param input_array: Исходный список.
-        :return: Отфильтрованный список.
-        """
-        pass
+        return sorted(res)
