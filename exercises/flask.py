00719b618c2b8f3d62e49291609b70e97a75173f
glebmiller <gleb.miller.ge@gmail.com>
2022-09-16 13:01:41 +0400

    updated .gitignore

--- exercises/flask.py
+++ exercises/flask.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+dict_of_users = dict()
 
 
 class FlaskExercise:
@@ -28,4 +34,54 @@ class FlaskExercise:
 
     @staticmethod
     def configure_routes(app: Flask) -> None:
-        pass
+        @app.route("/user", methods=["POST"])
+        def user():
+            request_data = request.get_json()
+            if "name" in request_data:
+                name = request_data["name"]
+                request_data.pop("name")
+                dict_of_users[name] = request_data
+                text = {"data": f"User {name} is created!"}
+                response = app.response_class(
+                    response=json.dumps(text), status=201, mimetype="application/json"
+                )
+                return response
+            else:
+                text = {"errors": {"name": "This field is required"}}
+                response = app.response_class(
+                    response=json.dumps(text), status=422, mimetype="application/json"
+                )
+                return response
+
+        @app.route("/user/<user_id>", methods=["GET", "POST", "DELETE"])
+        def user_modify(user_id):
+            if request.method == "GET":
+                if user_id in dict_of_users:
+                    text = {"data": f"My name is {user_id}"}
+
+                    response = app.response_class(
+                        response=json.dumps(text), status=200, mimetype="application/json"
+                    )
+                    return response
+
+            if request.method == "PATCH":
+
+                request_data = request.get_json()
+                new_name = request_data["name"]
+                dict_of_users[new_name] = dict_of_users[user_id]
+                dict_of_users[user_id].pop()
+
+                text = {"data": f"My name is {new_name}"}
+                response = app.response_class(
+                    response=json.dumps(text), status=200, mimetype="application/json"
+                )
+                return response
+
+            if request.method == "DELETE":
+                dict_of_users[user_id].pop()
+                response = app.response_class(status=204)
+                return response
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
