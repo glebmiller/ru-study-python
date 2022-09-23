a7fff3a4395fc20ee242fd496d3ddf724563522b
glebmiller <gleb.miller.ge@gmail.com>
2022-09-23 17:39:40 +0400

    fixed naming errors

--- exercises/map.py
+++ exercises/map.py
@@ -1,19 +1,23 @@
 from typing import Union
-from functools import reduce
 
 
 class MapExercise:
     @staticmethod
     def rating(list_of_movies: list[dict]) -> float:
-        def func(movie: dict) -> float:
-            if "," in movie["country"] and movie["rating_kinopoisk"] not in ("", "0"):
+        def positive_rating(movie: dict) -> float:
+            if movie["rating_kinopoisk"] == "":
+                movie["rating_kinopoisk"] = 0
+            else:
+                movie["rating_kinopoisk"] = float(movie["rating_kinopoisk"])
+
+            if "," in movie["country"] and movie["rating_kinopoisk"] != (0.0):
                 return float(movie["rating_kinopoisk"])
             else:
                 return None
 
-        list_of_floats = list(map(func, list_of_movies))
+        list_of_floats = list(map(positive_rating, list_of_movies))
         list_of_floats_filtered = list(filter(lambda x: x is not None, list_of_floats))
-        return reduce(lambda x, y: x + y, list_of_floats_filtered) / len(list_of_floats_filtered)
+        return sum(list_of_floats_filtered) / len(list_of_floats_filtered)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -24,4 +28,4 @@ class MapExercise:
                 return 0
 
         list_of_letter_counts = map(count_i, list_of_movies)
-        return reduce(lambda x, y: x + y, list_of_letter_counts)
+        return sum(list_of_letter_counts)
